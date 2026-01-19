ffb674dceb353b75cc1e6e04789d9c9db6b31edb
anupamyadav7528 <anupamyadav681209652710@gmail.com>
2026-01-19 07:24:25 +0530

    redused error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Shop from "./Shop";  // यह आपकी पुरानी 127 लाइन वाली दुकान है
-import Admin from "./Admin"; // यह प्रोडक्ट अपलोड करने वाला फॉर्म है
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Shop from "./Shop";
+import Admin from "./Admin";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* जब कोई वेबसाइट खोलेगा, उसे दुकान (Shop) दिखेगी */}
         <Route path="/" element={<Shop />} />
-        
-        {/* जब कोई '/admin' लिखेगा, उसे फॉर्म दिखेगा */}
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
